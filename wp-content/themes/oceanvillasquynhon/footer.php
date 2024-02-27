f49fce323a8afa42f0ea62ecbf2b0b155d3a3b7f
hailm <hai.lemanh.1310@gmail.com>
2024-02-27 11:45:31 +0700

    feat: footer

--- wp-content/themes/oceanvillasquynhon/footer.php
+++ wp-content/themes/oceanvillasquynhon/footer.php
@@ -1,1 +1,100 @@
-<?php
+<footer class="footer uk-light">
+    <div class="footer__top uk-text-center">
+        <div class="uk-container">
+            <figure class="footer__logo"><a href="<?= esc_url(home_url('/')) ?>"><img class="lazy" data-src="<?= get_theme_file_uri('/assets/images/Logo-Theoceanvillas.png') ?>" alt=""></a></figure>
+        </div>
+    </div>
+    <div class="footer__center footer__border uk-section-small">
+        <div class="uk-container">
+            <div class="uk-child-width-expand@m" uk-grid>
+                <div class="uk-width-auto@m">
+                    <div class="width-264px">
+                        <h4 class="footer__title">GET IN TOUCH</h4>
+                        <p class="footer__address">Nhon Ly - Cat Tien beach tourist area, Cat Tien commune, Phu Cat district, Binh Dinh, Vietnam</p>
+                    </div>
+                </div>
+                <div>
+                    <h4 class="footer__title">our Brands</h4>
+                    <ul class="footer__nav uk-nav uk-nav-default">
+                        <li><a href="#">Fusion Resort</a></li>
+                        <li><a href="#">Fusion Suites</a></li>
+                        <li><a href="#">Fusion Original</a></li>
+                        <li><a href="#">Hiive</a></li>
+                        <li><a href="#">Glow</a></li>
+                        <li><a href="#">Maia</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 class="footer__title">Info</h4>
+                    <ul class="footer__nav uk-nav uk-nav-default">
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Our story</a></li>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">Blogs</a></li>
+                        <li><a href="#">General terms</a></li>
+                        <li><a href="#">Privacy policy</a></li>
+                        <li><a href="#">Payment Methods</a></li>
+                    </ul>
+                </div>
+                <div class="uk-width-auto@m">
+                    <div class="width-453px">
+                        <h4 class="footer__title">Media</h4>
+                        <form class="uk-grid-small" uk-grid>
+                            <div class="uk-width-1-1">
+                                <div class="footer__form__label">Sign Up for Newsletter</div>
+                            </div>
+                            <div class="uk-width-expand">
+                                <input class="uk-input footer__form__input" type="email" placeholder="Enter your email" aria-label="Enter your email">
+                            </div>
+                            <div class="uk-width-auto">
+                                <button class="uk-button footer__form__btn uk-button-default">Suscribe</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer__bottom footer__border">
+        <div class="uk-container">
+            <div class="uk-flex-middle" uk-grid>
+                <div class="uk-width-expand">
+                    <div class="footer__bottom__txt">© Copyright 2024 The Ocean Villas. All rights reserved.</div>
+                </div>
+                <div class="uk-width-auto@s">
+                    <div class="uk-grid-small" uk-grid>
+                        <div>
+                            <a href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="29" viewBox="0 0 28 29" fill="none">
+                                    <rect y="0.225098" width="28" height="28" rx="4" fill="white" fill-opacity="0.2"/>
+                                    <path d="M15.1384 21.2251V14.839H17.3898L17.7269 12.3503H15.1383V10.7613C15.1383 10.0407 15.3485 9.54971 16.4338 9.54971L17.818 9.54909V7.32315C17.5786 7.29288 16.7569 7.2251 15.801 7.2251C13.8052 7.2251 12.4388 8.3849 12.4388 10.5149V12.3503H10.1816V14.839H12.4388V21.225H15.1384V21.2251Z" fill="white"/>
+                                </svg>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="29" viewBox="0 0 28 29" fill="none">
+                                    <rect y="0.225098" width="28" height="28" rx="4" fill="white" fill-opacity="0.2"/>
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M7 14.2251C7 11.4289 7 10.0308 7.66632 9.02375C7.96386 8.57407 8.34897 8.18896 8.79865 7.89141C9.80567 7.2251 11.2038 7.2251 14 7.2251C16.7962 7.2251 18.1943 7.2251 19.2013 7.89141C19.651 8.18896 20.0361 8.57407 20.3337 9.02375C21 10.0308 21 11.4289 21 14.2251C21 17.0213 21 18.4194 20.3337 19.4264C20.0361 19.8761 19.651 20.2612 19.2013 20.5588C18.1943 21.2251 16.7962 21.2251 14 21.2251C11.2038 21.2251 9.80567 21.2251 8.79865 20.5588C8.34897 20.2612 7.96386 19.8761 7.66632 19.4264C7 18.4194 7 17.0213 7 14.2251ZM17.6239 14.2254C17.6239 16.2269 16.0013 17.8494 13.9999 17.8494C11.9984 17.8494 10.3759 16.2269 10.3759 14.2254C10.3759 12.224 11.9984 10.6014 13.9999 10.6014C16.0013 10.6014 17.6239 12.224 17.6239 14.2254ZM13.9999 16.6233C15.3242 16.6233 16.3978 15.5497 16.3978 14.2254C16.3978 12.9011 15.3242 11.8275 13.9999 11.8275C12.6756 11.8275 11.602 12.9011 11.602 14.2254C11.602 15.5497 12.6756 16.6233 13.9999 16.6233ZM17.7671 11.2708C18.2374 11.2708 18.6187 10.8895 18.6187 10.4192C18.6187 9.94894 18.2374 9.5677 17.7671 9.5677C17.2968 9.5677 16.9156 9.94894 16.9156 10.4192C16.9156 10.8895 17.2968 11.2708 17.7671 11.2708Z" fill="white"/>
+                                </svg>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="29" viewBox="0 0 28 29" fill="none">
+                                    <rect y="0.225098" width="28" height="28" rx="4" fill="white" fill-opacity="0.2"/>
+                                    <path d="M14.341 19.6537L11.1815 19.5946C10.1585 19.5741 9.13301 19.6151 8.1301 19.402C6.60444 19.0834 6.49635 17.5217 6.38326 16.2118C6.22742 14.3701 6.28775 12.495 6.58183 10.6688C6.74785 9.64403 7.40121 9.03256 8.41161 8.96602C11.8224 8.72454 15.2559 8.75316 18.6592 8.86582C19.0186 8.87615 19.3805 8.9326 19.735 8.99685C21.4844 9.31023 21.5271 11.08 21.6405 12.5698C21.7536 14.075 21.7058 15.5879 21.4897 17.0828C21.3162 18.3205 20.9844 19.3585 19.5841 19.4587C17.8297 19.5897 16.1154 19.6952 14.356 19.6616C14.3561 19.6537 14.346 19.6537 14.341 19.6537ZM12.4835 16.52C13.8057 15.7443 15.1026 14.9814 16.4172 14.2109C15.0926 13.4351 13.7981 12.6723 12.4835 11.9017V16.52Z" fill="white"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+<!--/app-->
+<?php wp_footer(); ?>
+</body>
+</html>
--- wp-content/themes/oceanvillasquynhon/header.php
+++ wp-content/themes/oceanvillasquynhon/header.php
@@ -1,1 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="REFRESH" content="1800"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+    <?php wp_head(); ?>
+
+    <!--CSS-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.18.3/dist/css/uikit.min.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/assets/style.css?v=<?php echo(time()) ?>">
+
+    <!--JS-->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.18.3/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.18.3/dist/js/uikit-icons.min.js"></script>
+    <script src="<?php bloginfo('template_directory') ?>/assets/js/app.js?v=<?php echo(time()) ?>"></script>
+</head>
+<body <?php body_class(); ?>>
+<!--app-->
+<div id="app" class="uk-height-viewport uk-offcanvas-content uk-overflow-hidden uk-position-relative">
