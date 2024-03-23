f13ce30017bcaee795d99d82bc0aabd675e09fa0
hailm <hai.lemanh.1310@gmail.com>
2024-03-24 04:03:44 +0700

    Update page-offer.php

--- wp-content/themes/oceanvillasquynhon/templates/page-offer.php
+++ wp-content/themes/oceanvillasquynhon/templates/page-offer.php
@@ -57,7 +57,7 @@ get_header(); ?>
             ));
             if ($query->have_posts()): ?>
             <ul class="js-filter uk-child-width-1-1 uk-grid-84-l item-64px" uk-grid uk-scrollspy="target: .anima; repeat: false; cls: animate; delay: 100">
-                <?php while ($query->have_posts()){ $query->the_post(); ?>
+                <?php $num = 1; while ($query->have_posts()){ $query->the_post(); ?>
 
                 <?php
                 $id = get_the_ID();
@@ -85,7 +85,7 @@ get_header(); ?>
                                     <img class="lazy uk-transition-scale-up uk-transition-opaque" data-src="<?= $featured_img_url ?>" alt="<?= $img_alt ?>" uk-cover="">
                                     <canvas width="540" height="322"></canvas>
                                     <div class="uk-position-bottom offer__overlay"></div>
-                                    <button id="btn-<?php echo $terms[0]->slug; ?>" class="offer__btnView uk-button uk-button-default uk-position-bottom-right uk-position-small" type="button" uk-toggle="target: #toggle-usage-<?php echo $terms[0]->slug; ?>; animation: uk-animation-fade; queued: true">
+                                    <button id="btn-<?php echo $terms[0]->slug.$num; ?>" class="offer__btnView uk-button uk-button-default uk-position-bottom-right uk-position-small" type="button" uk-toggle="target: #toggle-usage-<?php echo $terms[0]->slug.$num; ?>; animation: uk-animation-fade; queued: true">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                             <path d="M6.90075 8.69998L11.7007 17.52C11.7607 17.64 11.9407 17.64 12.0007 17.52L16.8607 8.75998C16.9207 8.63998 16.8007 8.45998 16.6207 8.51998L14.7607 9.35998C14.5807 9.41998 13.0207 10.14 11.8207 10.62C10.6207 10.08 9.18075 9.41998 8.94075 9.29998L7.08075 8.45998C7.02075 8.39998 6.84075 8.57998 6.90075 8.69998Z" fill="white"/>
                                             <path d="M20.8204 12C20.8204 16.86 16.8604 20.82 12.0004 20.82C7.14039 20.82 3.18039 16.86 3.18039 12C3.18039 7.14002 7.14039 3.18002 12.0004 3.18002C16.8604 3.18002 20.8204 7.14002 20.8204 12ZM21.6004 12C21.6004 6.72002 17.2804 2.40002 12.0004 2.40002C6.72039 2.40002 2.40039 6.72002 2.40039 12C2.40039 17.28 6.72039 21.6 12.0004 21.6C17.2804 21.6 21.6004 17.28 21.6004 12Z" fill="white"/>
@@ -102,18 +102,18 @@ get_header(); ?>
                                         ?>
                                     </button>
                                 </div>
-                                <div class="offer__contentBox uk-card uk-card-body" hidden id="toggle-usage-<?php echo $terms[0]->slug; ?>"><?php the_content(); ?></div>
+                                <div id="toggle-usage-<?php echo $terms[0]->slug.$num; ?>" class="offer__contentBox uk-card uk-card-body" hidden><?php the_content(); ?></div>
                                 <script>
 
-                                    UIkit.util.on('#toggle-usage-<?php echo $terms[0]->slug; ?>', 'show', function () {
+                                    UIkit.util.on('#toggle-usage-<?php echo $terms[0]->slug.$num; ?>', 'show', function () {
                                         // do something
-                                        console.log('#toggle-usage-<?php echo $terms[0]->slug; ?> show');
-                                        document.querySelector("#btn-<?php echo $terms[0]->slug; ?>").classList.add('open');
+                                        console.log('#toggle-usage-<?php echo $terms[0]->slug.$num; ?> show');
+                                        document.querySelector("#btn-<?php echo $terms[0]->slug.$num; ?>").classList.add('open');
                                     });
-                                    UIkit.util.on('#toggle-usage-<?php echo $terms[0]->slug; ?>', 'hide', function () {
+                                    UIkit.util.on('#toggle-usage-<?php echo $terms[0]->slug.$num; ?>', 'hide', function () {
                                         // do something
-                                        console.log('#toggle-usage-<?php echo $terms[0]->slug; ?> hide');
-                                        document.querySelector("#btn-<?php echo $terms[0]->slug; ?>").classList.remove('open');
+                                        console.log('#toggle-usage-<?php echo $terms[0]->slug.$num; ?> hide');
+                                        document.querySelector("#btn-<?php echo $terms[0]->slug.$num; ?>").classList.remove('open');
                                     });
                                 </script>
                             </div>
@@ -142,7 +142,7 @@ get_header(); ?>
                         </div>
                     </div>
                 </li>
-                <?php } ?>
+                <?php $num++; } ?>
             </ul>
             <?php endif; wp_reset_postdata(); ?>
 
